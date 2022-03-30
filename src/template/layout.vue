5a97f898114f7b0ab30f050b86617435230d6cfd
weixiao <1130287243@qq.com>
2022-03-30 17:58:41 +0800

    fix: highlghtjs css 引入

--- src/template/layout.vue
+++ src/template/layout.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="van-doc">
-    <!-- <Button>fdfd</Button> -->
-    <HeaderComponent :header="header"/>
-    <NavComponent :navs="navs"/>
+    <HeaderComponent :header="header" />
+    <NavComponent :navs="navs" />
     <div class="van-doc-container van-doc-row">
       <div class="van-doc-content van-doc-content--common">
         <router-view></router-view>
@@ -15,10 +14,8 @@
 import NavComponent from "vite-plugin-vue-docs-plus/dist/template/nav.vue";
 import HeaderComponent from "vite-plugin-vue-docs-plus/dist/template/header.vue";
 import "vite-plugin-vue-docs-plus/dist/template/style.css";
-import 'highlight.js/styles/darcula.css'
-import 'ant-design-vue/dist/antd.css';
-// import {Button} from 'ant-design-vue' 
-// TODO: antdv 无法使用
+import "highlight.js/styles/default.css";
+import "ant-design-vue/dist/antd.css";
 
 export default {
   components: { HeaderComponent, NavComponent },
